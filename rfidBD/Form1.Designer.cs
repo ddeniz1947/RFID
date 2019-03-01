8d2481916418f1ddd9d73751ce04f317b539e690
ddeniz1947 <ddeniz1947@gmail.com>
2019-03-01 21:55:42 +0300

    başlandı

---
+++ rfidBD/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace rfidBD
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Ad = new System.Windows.Forms.Label();
+            this.txt_ad = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Soyad = new System.Windows.Forms.Label();
+            this.txt_uid = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.txt_soyad = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Ad
+            // 
+            this.Ad.AutoSize = true;
+            this.Ad.Location = new System.Drawing.Point(285, 70);
+            this.Ad.Name = "Ad";
+            this.Ad.Size = new System.Drawing.Size(20, 13);
+            this.Ad.TabIndex = 0;
+            this.Ad.Text = "Ad";
+            // 
+            // txt_ad
+            // 
+            this.txt_ad.Location = new System.Drawing.Point(349, 70);
+            this.txt_ad.Name = "txt_ad";
+            this.txt_ad.Size = new System.Drawing.Size(100, 20);
+            this.txt_ad.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(100, 114);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Soyad
+            // 
+            this.Soyad.AutoSize = true;
+            this.Soyad.Location = new System.Drawing.Point(285, 104);
+            this.Soyad.Name = "Soyad";
+            this.Soyad.Size = new System.Drawing.Size(37, 13);
+            this.Soyad.TabIndex = 4;
+            this.Soyad.Text = "Soyad";
+            // 
+            // txt_uid
+            // 
+            this.txt_uid.Location = new System.Drawing.Point(349, 136);
+            this.txt_uid.Name = "txt_uid";
+            this.txt_uid.Size = new System.Drawing.Size(100, 20);
+            this.txt_uid.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(100, 159);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM4";
+            // 
+            // txt_soyad
+            // 
+            this.txt_soyad.Location = new System.Drawing.Point(349, 101);
+            this.txt_soyad.Name = "txt_soyad";
+            this.txt_soyad.Size = new System.Drawing.Size(100, 20);
+            this.txt_soyad.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(285, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "rfid";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 227);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_soyad);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.txt_uid);
+            this.Controls.Add(this.Soyad);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_ad);
+            this.Controls.Add(this.Ad);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Ad;
+        private System.Windows.Forms.TextBox txt_ad;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label Soyad;
+        private System.Windows.Forms.TextBox txt_uid;
+        private System.Windows.Forms.Button button3;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox txt_soyad;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ rfidBD/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace rfidBD
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-6HE2FV2\\SQLEXPRESS;Initial Catalog=rfid;Integrated Security=True");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = baglanti;
+            
+            if(baglanti.State ==ConnectionState.Closed)
+            {
+                baglanti.Open();
+            }
+            string s1 = txt_uid.Text;
+            string s2 = txt_ad.Text;
+            string s3 = txt_soyad.Text;
+            cmd.CommandText = "INSERT INTO rfid_table2(input,ad,soyad) VALUES ('" + s1 + "' , '" + s2 + "' , '" +s3 + "')";
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            MessageBox.Show("Enes");
+          
+        }
+
+        
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+        
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            serialPort1.Open();
+            string entrada = serialPort1.ReadLine();
+            serialPort1.Close();
+            txt_uid.Text = entrada;
+        }
+    }
+}
